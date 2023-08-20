9bab72596f52dbcc03657b5275e6cf7683350763
AllanMuhari <allanmuhari087@gmail.com>
2023-08-21 01:06:40 +0300

    navbar

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
           src='https://cdn-icons-png.flaticon.com/512/95/95145.png?w=826&t=st=1691938209~exp=1691938809~hmac=c6c6b70736ad72ebcac635233a3137ad8838595aab7116bdadaa25c65829c586'
           alt=''
         ></img>
-        <h1 className='text-3xl font-bold text-black m-3'>
+        <h1 className='  flex text-2xl lg:text-3xl font-bold text-black m-3'>
           Rent a ride with us
         </h1>
       </div>
@@ -35,7 +35,7 @@ const Nav = () => {
       <ul
         className={
           nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#cea7e2] ease-in-out duration-500"
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#1e1b4b] ease-in-out duration-500"
             : "ease-in-out duration-500 fixed left-[-100%]"
         }
       >
@@ -46,14 +46,24 @@ const Nav = () => {
         <li className='p-4 border-b border-gray-600'>Company</li>
         <li className='p-4 border-b border-gray-600'>Resources</li>
         <li className='p-4 border-b border-gray-600'>About</li>
-        <li className='p-4'>Contact</li>
+        <li className='p-4 border-b border-gray-600'>Contact</li>
+        <div className='flex p-5 m-3 g-4'>
+          <button className='bg-blue-700 hover:bg-blue-800 rounded py-2'>
+            Sign up
+          </button>
+          <button className='bg-blue-700 hover:bg-blue-800 rounded py-2'>
+            Sign in
+          </button>
+        </div>
       </ul>
-      <button className='bg-blue-700 hover:bg-blue-800 rounded py-2'>
-        Sign up
-      </button>
-      <button className='bg-blue-700 hover:bg-blue-800 rounded py-2'>
-        Sign in
-      </button>
+      <div className='hidden md:flex lg:flex'>
+        <button className='bg-blue-700 hover:bg-blue-800 rounded py-2'>
+          Sign up
+        </button>
+        <button className='bg-blue-700 hover:bg-blue-800 rounded py-2'>
+          Sign in
+        </button>
+      </div>
     </div>
   );
 };
