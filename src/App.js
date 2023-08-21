342feddde3b7794aed28804da72f451f232f4dab
AllanMuhari <allanmuhari087@gmail.com>
2023-08-21 16:01:39 +0300

    location tab

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 
 import Nav from "./Components/Nav/Nav";
 import Context from "./Components/Content/Context";
+import Location from "./Components/Location/Location";
 
 function App() {
   return (
     <div>
       <Nav />
       <Context />
+      <Location />
     </div>
   );
 }
---
+++ src/Components/Location/Location.jsx
@@ -0,0 +1,27 @@
+/** @format */
+
+import React from "react";
+
+const Location = () => {
+  return (
+    <div className='bg-white max-w-[1640px] mt-5 h-44 rounded-lg flex lg:ml-[120px]'>
+      <div className='m-auto'>
+        <h1 className='flex text-1xl font-bold text-gray-700 w-2 '>LOCATION</h1>
+        <input
+          type='text'
+          className='border-b border-gray-600 text-black w-24'
+        ></input>
+      </div>
+      <div className='m-auto'>
+        <h1 className='flex text-1xl font-bold text-gray-700'>PICKUP DATE</h1>
+        <input type='date'></input>
+      </div>
+      <div className='m-auto'>
+        <h1 className='flex text-1xl font-bold text-gray-700 '>RETRUN DATE</h1>
+        <input type='date'></input>
+      </div>
+    </div>
+  );
+};
+
+export default Location;
