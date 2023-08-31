90f00efae736191c7b164b3a9ad74bed3c392557
AllanMuhari <allanmuhari087@gmail.com>
2023-08-31 15:33:17 +0300

    Hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='bg-slate-200 w-auto h-auto max-w-[1640px] lg:m-auto  p-3  mx-2'>
-        <div className='flex justify-center'>
+    <div className='bg-slate-200 w-auto h-auto max-w-[1640px] lg:m-auto rounded-md  p-3  mx-2 mt-4'>
+        <div className='flex justify-center '>
             <h1>
                 Why choose us
             </h1>
